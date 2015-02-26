3749eac894c432d3a1272ae08f8c39c67e405bf6
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-02-26 13:17:09 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var createDB = function(){};
+var express = require('express');
+var app =express();
+var createDB = function(){
+  
+};
+var start = function(){
+  app.listen(3000);
+  app.on('disconnect',)
+};
