5bd82af2302e97f141a2d747751202cd0e043eef
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-03-25 19:09:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app =express();
-var createDB = function(){
+var db = function(name,rows,columns){
   
+};
+var createDB = function(dbName){
+   
 };
 var start = function(){
   app.listen(3000);
